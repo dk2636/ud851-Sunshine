c0353aad235b88c86eb4542a5826c953682c49da
Jamesk <dimitris.kokorogiannis@intrasoft-intl.com>
2018-05-01 13:26:48 +0300

    Initial set up based on JamesK previous activity for  S02.03-Exercise

--- S02.03-Exercise-Polish/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S02.03-Exercise-Polish/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -15,26 +15,36 @@
  */
 package com.example.android.sunshine;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.utilities.NetworkUtils;
 import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
 
+import org.json.JSONException;
+
+import java.io.IOException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    final  static String LOG_TAG = MainActivity.class.getSimpleName();
+
     private TextView mWeatherTextView;
 
     // TODO (6) Add a TextView variable for the error message display
-
+    private ProgressBar mProgressBarView;
     // TODO (16) Add a ProgressBar variable to show and hide the progress bar
 
     @Override
@@ -50,8 +60,8 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO (7) Find the TextView for the error message using findViewById
 
-        // TODO (17) Find the ProgressBar using findViewById
-
+        // Get a reference to the ProgressBar using findViewById
+        mProgressBarView = (ProgressBar) findViewById(R.id.pb_loading_indicator);
         /* Once all of our views are setup, we can load the weather data. */
         loadWeatherData();
     }
@@ -61,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
      * background method to get the weather data in the background.
      */
     private void loadWeatherData() {
-        // TODO (20) Call showWeatherDataView before executing the AsyncTask
+        //String location ="Athens,gr";
         String location = SunshinePreferences.getPreferredWeatherLocation(this);
         new FetchWeatherTask().execute(location);
     }
@@ -71,13 +81,17 @@ public class MainActivity extends AppCompatActivity {
     // TODO (9) Create a method called showErrorMessage that will hide the weather data and show the error message
 
     public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
-
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressBarView.setVisibility(View.VISIBLE);
+        }
         // TODO (18) Within your AsyncTask, override the method onPreExecute and show the loading indicator
 
         @Override
         protected String[] doInBackground(String... params) {
 
-            /* If there's no zip code, there's nothing to look up. */
+            /* If there's no input param, there's nothing to look up. */
             if (params.length == 0) {
                 return null;
             }
@@ -85,33 +99,33 @@ public class MainActivity extends AppCompatActivity {
             String location = params[0];
             URL weatherRequestUrl = NetworkUtils.buildUrl(location);
 
+            String[] simpleJsonWeatherData = new String[0];
             try {
-                String jsonWeatherResponse = NetworkUtils
-                        .getResponseFromHttpUrl(weatherRequestUrl);
+                String weatherSearchResults = NetworkUtils.getResponseFromHttpUrl(searchURL);
+                Log.i(LOG_TAG, "json data: " + weatherSearchResults);
+                simpleJsonWeatherData = OpenWeatherJsonUtils
+                        .getSimpleWeatherStringsFromJson(MainActivity.this, weatherSearchResults);
 
-                String[] simpleJsonWeatherData = OpenWeatherJsonUtils
-                        .getSimpleWeatherStringsFromJson(MainActivity.this, jsonWeatherResponse);
-
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "IOException"+e);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, "JSONException"+e);
+            } finally {
                 return simpleJsonWeatherData;
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
             }
         }
 
         @Override
-        protected void onPostExecute(String[] weatherData) {
+        protected void onPostExecute(String[] s) {
             // TODO (19) As soon as the data is finished loading, hide the loading indicator
-
-            if (weatherData != null) {
-                // TODO (11) If the weather data was not null, make sure the data view is visible
+            mProgressBarView.setVisibility(View.INVISIBLE);
+            if (s != null && !(s.length == 0) ) {
                 /*
                  * Iterate through the array and append the Strings to the TextView. The reason why we add
                  * the "\n\n\n" after the String is to give visual separation between each String in the
                  * TextView. Later, we'll learn about a better way to display lists of data.
                  */
-                for (String weatherString : weatherData) {
+                for (String weatherString : s) {
                     mWeatherTextView.append((weatherString) + "\n\n\n");
                 }
             }
@@ -132,14 +146,25 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Search" menu option
+            case R.id.action_refresh:
 
-        if (id == R.id.action_refresh) {
+                //clear the screen
             mWeatherTextView.setText("");
+                // show a Toast message, that the refresh option was selected
+                Context context = getApplicationContext();
+                CharSequence text = "Refresh item selected: " + R.id.action_refresh;
+                Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+                toast.show();
+                // re-load the weather data
             loadWeatherData();
             return true;
+            default:
+                /* an unknown menu item Id was passed */
+                return super.onOptionsItemSelected(item);
         }
 
-        return super.onOptionsItemSelected(item);
     }
 }
